f3bfd5a0791d28fc0066ebd6c98a0020071c01af
Fro-Q <285695133@qq.com>
2024-07-27 15:54:58 +0800

    Fix bug: Invalid date.

--- docs/.vitepress/theme/components/posts.data.js
+++ docs/.vitepress/theme/components/posts.data.js
@@ -1,6 +1,6 @@
 // posts.data.js
-import { createContentLoader } from 'vitepress'
-import { getReadingTime } from '../utils/readTime'
+import { createContentLoader } from "vitepress";
+import { getReadingTime } from "../utils/readTime";
 
 export default createContentLoader("blog/*.md", {
   excerpt: true,
@@ -12,22 +12,22 @@ export default createContentLoader("blog/*.md", {
         frontmatter,
         url,
         excerpt,
-        date: formatDate(frontmatter.date),
-        readingInfo: getReadingTime(src)
+        date: formatDate(frontmatter.created),
+        readingInfo: getReadingTime(src),
       }))
-      .sort((a, b) => b.date.time - a.date.time)
-  }
-})
+      .sort((a, b) => b.date.time - a.date.time);
+  },
+});
 
 function formatDate(raw) {
-  const date = new Date(raw)
+  const date = new Date(raw);
 
   return {
     time: +date,
-    string: date.toLocaleDateString('zh-CH', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    })
-  }
+    string: date.toLocaleDateString("zh-CH", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  };
 }
