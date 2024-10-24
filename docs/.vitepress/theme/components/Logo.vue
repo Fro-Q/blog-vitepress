65731af093ef3aca6e60b4c20c9ea4503c5d3d2c
Fro-Q <285695133@qq.com>
2024-10-24 13:23:51 +0800

    Trying to fix the bug that vite build hangs forever.

--- docs/.vitepress/theme/components/Logo.vue
+++ docs/.vitepress/theme/components/Logo.vue
@@ -3,37 +3,47 @@ import { ref } from "vue";
 
 const props = defineProps(["blinking"]);
 
-const logoHead = ref("fr");
-const logoTail = ref("oQ");
-
-// if (props.blinking) {
-//   setInterval(() => {
-//     if (logoTail.value === "oQ") {
-//       logoTail.value = "-Q";
-//       setTimeout(() => {
-//         logoTail.value = "oQ";
-//       }, 300);
-//     } else {
-//       logoTail.value = "oQ";
-//     }
-//   }, 8000);
-// }
+const blinkingLetter = ref("o");
 </script>
 
 <template>
   <div class="group inline-block">
-    <span class="text duration-400 text-neutral-400 transition-opacity delay-500 group-hover:opacity-20">{{ logoHead }}</span>
+    <span class="text text-neutral-400 transition-opacity delay-100 duration-200 hover:delay-0 group-hover:opacity-20">fr</span>
     <span
-      class="text duration-400 inline-block text-end text-neutral-400 transition group-hover:text-emerald-400"
-      :class="props.blinking ? 'min-w-[1.3em]' : ''"
+      class="text inline-block text-end text-neutral-400 transition duration-200 group-hover:text-emerald-400"
+      :class="props.blinking ? 'blinking min-w-[0.5em]' : ''"
     >
-      {{ logoTail }}
+      {{ blinkingLetter }}
     </span>
+    <span class="text inline-block text-end text-neutral-400 transition duration-200 group-hover:text-emerald-400">Q</span>
   </div>
 </template>
 
 <style scoped>
 .text {
   font-family: "noteworthy", cursive;
+}
+
+@keyframes blink {
+  0% {
+    transform: scaleY(1);
+  }
+  48% {
+    transform: scaleY(1);
+  }
+  50% {
+    transform: scaleY(0);
+  }
+  52% {
+    transform: scaleY(1);
+  }
+  100% {
+    transform: scaleY(1);
+  }
+}
+
+.blinking {
+  transform-origin: 100% 60%;
+  animation: blink 8000ms infinite;
 }
 </style>
